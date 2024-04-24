8c4a1a3d63cff8048741567a60e39257e8754f7d
Diegodossantos23 <diegodossantos900@outlook.com>
2024-04-24 01:13:27 -0300

    change footer layout

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,81 +1,128 @@
 import React from 'react';
-import { IoLogoFacebook, IoLogoTwitter, IoLogoInstagram } from 'react-icons/io';
-import { IoMdMail } from 'react-icons/io';
-import { IoIosArrowUp } from 'react-icons/io';
-import styles from './Footer.module.scss'; // Estilo do rodapé
-import Logo from '../../assets/logo/hiperion-logo.svg';
+import styles from './Footer.module.scss'; // Importando os estilos SCSS modulares
 
-const Footer = () => {
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    };
+export default function App() {
+  return (
+    <footer className={styles.footer}>
+      <section className={styles.social}>
+       {/*<div className={styles.socialText}>
+          <span>Get connected with us on social networks:</span>
+  </div>*/}
 
-    return (
-        <footer className={styles.footer}>
-            <div className={styles.footerContent}>
-                <div className={styles.infoContainer}>
-                    <div className={styles.logoContainer}>
-                        <img src={Logo} alt="Logo" className={styles.logo} />
-                    </div>
-                    <div className={styles.location}>
-                        <h3>Localização 1</h3>
-                        <div>
-                            <p>Hiperion Serras - SUMARÉ</p>
-                            <p>R. Bruno Penteado Ferraz de Campos</p>
-                            <p>Jardim Manchester</p>
-                            <p>Sumaré - SP</p>
-                        </div>
-                        <h3>Localização 2</h3>
-
-                        <div>
-                            <p>Hiperion Serras - SÃO PAULO</p>
-                            <p>Avenida Pde Arlindo Vieira 1719</p>
-                            <p>Jardim Vergueiro (Sacoma)</p>
-                            <p>São Paulo SP</p>
-                        </div>
-                    </div>
-                    {/* <div className={styles.contact}>
-            <h3>Contato</h3>
-            <p>vendas@hiperionserras.com.br</p>
-            <p>www.hiperionserras.com.br</p>
-          </div> */}
-                 
-                </div>
-                <div className={styles.navLinks}>
-                    <h3>Links</h3>
-                    <a href="/">Início</a>
-                    <a href="/about">Sobre</a>
-                    <a href="/services">Serviços</a>
-                    <a href="/products">Produtos</a>
-                    <a href="/partners">Parceiros</a>
-                    <a href="/blog">Blog</a>
-                    <a href="/contact">Contato</a>
-                    <div className={styles.services}>
-                        <h3>Serviços</h3>
-                        <p>Serviço 1</p>
-                        <p>Serviço 2</p>
-                        <p>Serviço 3</p>
-                        <p>Serviço 4</p>
-                    </div>
-                </div>
-                <div className={styles.socialIcons}>
-          <a href="https://www.facebook.com"><IoLogoFacebook /></a>
-          <a href="https://www.twitter.com"><IoLogoTwitter /></a>
-          <a href="https://www.instagram.com"><IoLogoInstagram /></a>
-          <a href="mailto:example@example.com"><IoMdMail /></a>
+        <div className={styles.socialLinks}>
+          <a href='https://www.facebook.com' className={styles.socialLink}>
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href='https://twitter.com' className={styles.socialLink}>
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href='https://www.google.com' className={styles.socialLink}>
+            <i className="fab fa-google"></i>
+          </a>
+          <a href='https://www.instagram.com' className={styles.socialLink}>
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href='https://www.linkedin.com' className={styles.socialLink}>
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a href='https://github.com' className={styles.socialLink}>
+            <i className="fab fa-github"></i>
+          </a>
         </div>
-                <div className={styles.scrollTop} onClick={scrollToTop}>
-                    <IoIosArrowUp />
-                </div>
-            </div>
-            <div className={styles.copyRight}>
-                <p>&copy; 2024 Sua Empresa. Todos os direitos reservados.</p>
-            </div>
-        </footer>
-    );
-};
+      </section>
 
-export default Footer;
+      <section className={styles.footerContent}>
+        <div className={styles.footerColumn}>
+          <h6 className={styles.footerHeading}>
+            Hiperion Serras
+          </h6>
+          <p>
+            Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit.
+          </p>
+        </div>
+
+        <div className={styles.footerColumn}>
+          <h6 className={styles.footerHeading}>Produtos</h6>
+          <p>
+            <a href='#!' className={styles.footerLink}>
+            Lâminas de Serra Fita
+            </a>
+          </p>
+          <p>
+            <a href='#!' className={styles.footerLink}>
+            Serras Circulares
+            </a>
+          </p>
+          <p>
+            <a href='#!' className={styles.footerLink}>
+              Máquinas e Equipamentos
+            </a>
+          </p>
+          <p>
+            <a href='#!' className={styles.footerLink}>
+            Máquinas de corte
+            </a>
+          </p>
+        </div>
+
+        <div className={styles.footerColumn}>
+          <h6 className={styles.footerHeading}>Navegação</h6>
+          <p>
+            <a href='#!' className={styles.footerLink}>
+              Inicio
+            </a>
+          </p>
+          <p>
+            <a href='#!' className={styles.footerLink}>
+              Sobre
+            </a>
+          </p>
+          <p>
+            <a href='#!' className={styles.footerLink}>
+              Serviços
+            </a>
+          </p>
+          <p>
+            <a href='#!' className={styles.footerLink}>
+              Blog
+            </a>
+          </p>
+        </div>
+
+        <div className={styles.footerColumn}>
+          <h6 className={styles.footerHeading}>Contact</h6>
+          <p>
+            {/*<i className={`fas fa-home ${styles.footerIcon}`}></i>
+                Hiperion Serras - SUMARÉ
+            */}
+            </p>
+
+          <p>
+          R. Bruno Penteado Ferraz de
+                Campos
+                Jardim Manchester
+                Sumaré - SP
+          </p>
+          <p>
+            <i className={`fas fa-envelope ${styles.footerIcon}`}></i> vendas@hiperionserras.com.br
+          </p>
+          <p>
+            <i className={`fas fa-phone ${styles.footerIcon}`}></i> +55 (19) 99414-2300
+
+          </p>
+          {/*<p>
+            <i className={`fas fa-print ${styles.footerIcon}`}></i> + 01 234 567 89
+</p>*/}
+        </div>
+      </section>
+
+      <div className={styles.copyRight}>
+        © 2021 Copyright:
+        <a className={styles.footerLink} href=''>
+          Hiperion Serras
+        </a>
+      </div>
+    </footer>
+  );
+}
